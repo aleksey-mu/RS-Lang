2b78c92c044aa7ff662d81307d5d1b1c2c7e5ffa
aleksey-mu <pacuklex@mail.ru>
2020-07-15 00:43:26 +0300

    add: app settings page

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,48 @@
 import '../style/style.scss';
 import BurgerMenu from './header/burgerMenu';
 import Router from './router/router';
-import gameInit from './games/speakIt/speakItInit';
+import speakItInit from './games/speakIt/speakItInit';
 import SavannahGame from './games/savannah-game/game';
+import settingsInit from './settings/settings';
 
 const myBurgerMenu = new BurgerMenu();
 myBurgerMenu.init();
 
-function mainPage() {
-	document.querySelector('main').innerHTML = '<h2>Main Page</h2>';
-}
+function mainPage() {}
 function wordsPage() {
 	document.querySelector('.container-fluid').style.background = 'green';
 }
 function trainSpeakItPage() {
-	gameInit();
+	speakItInit();
+}
+function settingsPage() {
+	settingsInit();
 }
 function trainingSavannahPage() {
-  const savannahGame = new SavannahGame('#savannah-game', '#/training/savannah/');
+	const savannahGame = new SavannahGame(
+		'#savannah-game',
+		'#/training/savannah/'
+	);
 
-  const toMainPage = () => {
-    window.location.hash = '/main/';
-  }
+	const toMainPage = () => {
+		window.location.hash = '/main/';
+	};
 
-  const getStatistic = (statistic) => {
-    console.log(statistic);
-  }
+	const getStatistic = (statistic) => {
+		console.log(statistic);
+	};
 
-  savannahGame.init();
-  savannahGame.onGameClose(toMainPage);
-  savannahGame.onGameEnd(getStatistic);
+	savannahGame.init();
+	savannahGame.onGameClose(toMainPage);
+	savannahGame.onGameEnd(getStatistic);
 }
-
 
 const loadPage = {
 	mainPage,
 	wordsPage,
-  trainSpeakItPage,
-  trainingSavannahPage,
+	trainSpeakItPage,
+	trainingSavannahPage,
+	settingsPage,
 };
 
 const router = new Router(loadPage);
